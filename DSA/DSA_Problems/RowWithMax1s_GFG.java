98e1dc2b72853e50f01c833bd171f54a496e44ce
gauzam <gautam01428@gmail.com>
2024-01-31 13:30:46 +0530

    Solved RowWithMax1s
    TC: O(m*n)
    SC: O(1)

---
+++ DSA/DSA_Problems/RowWithMax1s_GFG.java
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            inputLine = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(inputLine[0]);
+            int m = Integer.parseInt(inputLine[1]);
+            int[][] arr = new int[n][m];
+            inputLine = br.readLine().trim().split(" ");
+
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    arr[i][j] = Integer.parseInt(inputLine[i * m + j]);
+                }
+            }
+            int ans = new Solution().rowWithMax1s(arr, n, m);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    int rowWithMax1s(int arr[][], int n, int m) {
+
+        //traversing over the matrix column by column
+        for(int j = 0; j < m; j++){
+            //keeping the column constant
+
+            for(int i = 0; i < n; i++){
+                //iterating from top to bottom
+
+                if(arr[i][j] == 1){
+                    //first elem that returns 1 - is the row with most 1's
+                    return i; //return that row
+                }
+            }
+        }
+
+        return -1;
+    }
+}
