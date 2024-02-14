92d7ae2f1427e3ad459cf66c477e5c27cb453cd7
gauzam <gautam01428@gmail.com>
2024-02-14 16:12:30 +0530

    Solved ValidAnagrams
    TC: O(n)
    SC: O(1)

---
+++ DSA/DSA_Problems/ValidAnagrams_Leetcode.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        if(s.length() != t.length()){
+            return false;
+        }
+
+        //creating a frequency array of fixed size
+        int arr[] = new int[26];
+
+        for(int i = 0; i < s.length(); i++){
+            arr[s.charAt(i) - 'a']++;
+        }
+
+        for(int i = 0; i < t.length(); i++){
+            arr[t.charAt(i) - 'a']--;
+        }
+
+        for(int i = 0; i < arr.length; i++){
+            //any non zero value found means not anagrams
+            if(arr[i] != 0){
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
