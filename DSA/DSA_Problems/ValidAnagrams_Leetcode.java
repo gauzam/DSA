6a65e13292f4f1f6fb08c85e49b060d8c7bb445a
gauzam <gautam01428@gmail.com>
2024-03-08 14:05:59 +0530

    Update ValidAnagrams_Leetcode.java
    TC: O(n)
    SC: O(1)

--- DSA/DSA_Problems/ValidAnagrams_Leetcode.java
+++ DSA/DSA_Problems/ValidAnagrams_Leetcode.java
@@ -8,14 +8,17 @@ class Solution {
         //creating a frequency array of fixed size
         int arr[] = new int[26];
 
+        //updating freq array for string1
         for(int i = 0; i < s.length(); i++){
             arr[s.charAt(i) - 'a']++;
         }
 
+        //updating freq array for string2
         for(int i = 0; i < t.length(); i++){
             arr[t.charAt(i) - 'a']--;
         }
 
+        //for it to be anagrams, the freq array should be all zeroes
         for(int i = 0; i < arr.length; i++){
             //any non zero value found means not anagrams
             if(arr[i] != 0){
