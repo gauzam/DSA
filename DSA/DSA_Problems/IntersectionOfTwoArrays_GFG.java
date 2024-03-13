2f692416a05e07ee3db9258e29a56548084da3d6
gauzam <gautam01428@gmail.com>
2024-03-13 23:13:05 +0530

    Solved IntersectionOfTwoArrays
    TC: O(n)
    SC: O(n)

---
+++ DSA/DSA_Problems/IntersectionOfTwoArrays_GFG.java
@@ -0,0 +1,81 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+class GFG {
+    public static void main(String[] args) {
+
+        // Taking input using class Scanner
+        Scanner sc = new Scanner(System.in);
+
+        // Taking count of total testcases
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n, m;
+
+            // taking count of elements in array a
+            n = sc.nextInt();
+
+            // taking count of elements in array b
+            m = sc.nextInt();
+
+            // Creating 2 arrays of n and m
+            int a[] = new int[n];
+            int b[] = new int[m];
+
+            // inserting elements to array a
+            for (int i = 0; i < n; i++) {
+                a[i] = sc.nextInt();
+            }
+
+            // inserting elements to array b
+            for (int i = 0; i < m; i++) {
+                b[i] = sc.nextInt();
+            }
+            Solution ob = new Solution();
+            // calling NumberofElementsInIntersection method
+            // and printing the result
+            System.out.println(ob.NumberofElementsInIntersection(a, b, n, m));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    // Function to return the count of the number of elements in
+    // the intersection of two arrays.
+    public static int NumberofElementsInIntersection(int a[], int b[], int n, int m) {
+        // Your code here
+
+        HashSet<Integer> set = new HashSet<>();
+
+        //adding every elem from array1 into the hashset
+        for(int i = 0; i < n; i++){
+            set.add(a[i]);
+        }
+
+        int num = 0;
+
+        //for every elem in array2 we check if it existed in array1
+        for(int i = 0; i < m; i++){
+
+            //if it did, we increment counter and remove that key from the set
+            if(set.contains(b[i])){
+                set.remove(b[i]);
+                num++;
+            }
+
+        }
+
+        return num;
+
+    }
+};
